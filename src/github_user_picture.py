80772c3ead41e5724d25a664ef34b6fd66abdb32
Shoitii <shoiti.isotani@gmail.com>
2024-02-07 19:26:42 -0300

    new file

---
+++ src/github_user_picture.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+def get_picture_link(github_user):
+  page = requests.get(f'https://github.com/{github_user}')
+  site = bs(page.content, 'html.parser')
+  picture_link = site.find('img', {'class' : "avatar avatar-user width-full border color-bg-default"})['src']
+  return picture_link
+
+
+user = str(input("Input Github user: "))
+print(get_picture_link(user))
